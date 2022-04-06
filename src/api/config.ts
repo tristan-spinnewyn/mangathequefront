53768d2038dfcb54c498df9f2a52c8be73ce7131
Shin <tristan.spinnewyn@ik.me>
2022-04-07 00:24:48 +0200

    remove /

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,8 +1,9 @@
 import {getToken} from "../services/authService";
 
+
 export const HEADER = {
     headers: {
         authorization: "Bearer " + getToken()
     }
 }
-export const url_api = 'https://mangatheque-prod.herokuapp.com/'
+export const url_api = 'https://mangatheque-prod.herokuapp.com'
